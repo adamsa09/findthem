819417a3e57e597b184975aef67d9d04f72b8397
Adam Sarhan <adam09sarhan@gmail.com>
2022-01-11 23:58:11 -0500

    First commit

---
+++ findthem.py
@@ -0,0 +1,126 @@
+from bs4 import BeautifulSoup
+import requests
+from sys import argv
+
+
+print(
+    '''
+    ███████ ██ ███    ██ ██████  ████████ ██   ██ ███████ ███    ███ 
+    ██      ██ ████   ██ ██   ██    ██    ██   ██ ██      ████  ████ 
+    █████   ██ ██ ██  ██ ██   ██    ██    ███████ █████   ██ ████ ██ 
+    ██      ██ ██  ██ ██ ██   ██    ██    ██   ██ ██      ██  ██  ██ 
+    ██      ██ ██   ████ ██████     ██    ██   ██ ███████ ██      ██
+    '''
+)
+
+help_menu = '''
+Help
+
+NOTE: PLEASE PUT ALL OPTIONS IN DOUBLE QUOTES
+
+-h                 Display this menu
+-n <name>          Name of target
+-r <results>       Number of results to return
+OPTIONAL
+-s <site>          Site to look on
+-c <custom search> Search for a custom query NOTE: Only -r is allowed (and obligatory) when using custom query
+'''
+
+isSite = False
+isCustom = False
+
+if len(argv) == 1:
+    print(help_menu)
+    quit()
+else:
+    pass
+
+if '-h' in argv:
+    print(help_menu)
+    quit()
+else:
+    pass
+
+
+if '-s' in argv:
+    site = argv[argv.index('-s') + 1]
+    isSite = True
+else:
+    pass
+
+if '-c' in argv:
+    if len(argv) == 5:
+        custom = argv[argv.index('-c') + 1]
+        isCustom = True
+    else:
+        print(help_menu)
+        quit()
+else:
+    pass
+
+if '-n' in argv:
+    name = argv[(argv.index('-n') + 1)]
+else:
+    if isCustom == False:
+        print(help_menu)
+        quit()
+    else:
+        pass
+
+
+if '-r' in argv:
+    results = int(argv[argv.index('-r') + 1])
+else:
+    print(help_menu)
+    quit()
+
+
+def find(isCustom, isSite):
+    if isCustom == False:
+        query = f'"{name}"'
+        query = query.replace(' ', '+')
+        if isSite == False:
+            page = requests.get(
+                f'https://www.google.com/search?q={query}&num={results}')
+            soup = BeautifulSoup(page.text, 'html.parser')
+            links = soup.findAll('a')
+            for link in links:
+                link_href = link.get('href')
+                if 'url?q=' in link_href and not 'webcache' in link_href:
+                    link_href = link_href.replace('/url?q=', ' ')
+                    link_head, sep, tail = link_href.partition('&sa')
+                    print(f'[HIT] ' + ' ' + link_head + '\n' +
+                          '-------------------------------------------------------------------------------------------------')
+        else:
+            query = f'"{name}" site:"{site}"'
+            query = query.replace(' ', '+')
+            page = requests.get(
+                f'https://www.google.com/search?q={query}&num={results}')
+            soup = BeautifulSoup(page.text, 'html.parser')
+            links = soup.findAll('a')
+            for link in links:
+                link_href = link.get('href')
+                if 'url?q=' in link_href and not 'webcache' in link_href:
+                    link_href = link_href.replace('/url?q=', ' ')
+                    link_head, sep, tail = link_href.partition('&sa')
+                    print(f'[HIT] ' + ' ' + link_head + '\n' +
+                          '-------------------------------------------------------------------------------------------------')
+    else:
+        page = requests.get(
+            f'https://www.google.com/search?q={custom}&num={results}')
+        soup = BeautifulSoup(page.text, 'html.parser')
+        links = soup.findAll('a')
+        for link in links:
+            link_href = link.get('href')
+            if 'url?q=' in link_href and not 'webcache' in link_href:
+                link_href = link_href.replace('/url?q=', ' ')
+                link_head, sep, tail = link_href.partition('&sa')
+                print(f'[HIT] ' + ' ' + link_head + '\n' +
+                      '-------------------------------------------------------------------------------------------------')
+
+
+# try:
+find(isCustom=isCustom, isSite=isSite)
+# except:
+#     print('No results found')
+#     quit()
